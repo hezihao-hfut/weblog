c2854c0fcb6981e9c01f34cb397170332a9d06cd
hezihao <766560719@qq.com>
2025-05-20 15:05:27 +0800

    weblog

---
+++ weblog-springboot/weblog-module-common/src/main/java/com/quanxiaoha/weblog/common/domain/mapper/UserMapper.java
@@ -0,0 +1,13 @@
+package com.quanxiaoha.weblog.common.domain.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.quanxiaoha.weblog.common.domain.dos.UserDO;
+
+/**
+ * @author: 犬小哈
+ * @url: www.quanxiaoha.com
+ * @date: 2023-08-22 17:06
+ * @description: TODO
+ **/
+public interface UserMapper extends BaseMapper<UserDO> {
+}
